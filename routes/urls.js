26b3836a09b023354a64a0ade8bbdd104dbc4d12
Benjamin Hanimann <Witzelfitz@gmail.com>
2025-03-04 13:51:20 +0100

    ✅ Erweitere URL-Routen mit neuen Endpunkten für aktive URLs, Deaktivierung und Löschung

--- routes/urls.js
+++ routes/urls.js
@@ -7,16 +7,38 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', async (req, res) => {
+    const urls = await Url.find({ isActive: true });
+    res.send(urls);
+});
+
+router.get('/all', async (req, res) => {
     const urls = await Url.find();
     res.send(urls);
-}
-);
+});
 
 router.get('/:shortUrl', async (req, res) => {
     const url = await Url.findOne({ shortUrl: req.params.shortUrl });
     if (!url) return res.status(404).send('The url with the given shortUrl was not found');
-    res.send(url);
+
+    //if url is not active, return 404 it is outdated
+    if (!url.isActive) return res.status(404).send('The url with the given shortUrl is not active');
+
+    url.clicks++;
+    await url.save();
+
+    res.redirect(url.originalUrl);
 });
+
+router.put('/deactivate/:shortUrl', async (req, res) => {
+    const url = await Url.findOne({ shortUrl: req.params.shortUrl });
+    if (!url) return res.status(404).send('The url with the given shortUrl was not found');
+
+    url.isActive = false;
+    await url.save();
+
+    res.send(url);
+}
+);
 
 router.post('/', async (req, res) => {
     const { error } = validateUrl(req.body);
@@ -28,6 +50,15 @@ router.post('/', async (req, res) => {
 
     await url.save();
     res.send(url);
+});
+
+router.delete('/:shortUrl', async (req, res) => {
+    const url = await Url.findOne({ shortUrl: req.params.shortUrl });
+    if (!url) return res.status(404).send('The url with the given shortUrl was not found');
+    
+    await Url.findOneAndDelete({ shortUrl: req.params.shortUrl });
+    res.send(url);
+
 });
 
 export {router as urlsRouter};
